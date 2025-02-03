f0b078335f7265a21a5bf9b4d0ac56654224a561
Aditya <gjasunday@gmail.com>
2025-02-04 00:31:14 +0530

    sample

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,8 @@
 import Main from "@/app/Main/page";
+import Parallax from "./LandingPage/mainLandingPage";
 
 export default function Home() {
-  return(
-      <Main/>
+  return (
+    <Parallax />
   )
 }
