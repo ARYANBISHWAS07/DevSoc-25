cd7dcc8befc54e51a716721b3cbc973fc9027627
var-code-5 <varshithisgod@gmail.com>
2025-02-03 22:56:02 +0530

    feat: add ui for extension

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,5 +1,5 @@
+import { House, UserRoundPen } from 'lucide-react';
 import './App.css';
-
 
 function App() {
   const startCapture = () => {
@@ -19,10 +19,16 @@ function App() {
   };
 
   return (
-    <div className="w-[300px] h-[400px] flex flex-col items-center gap-4">
-      <button onClick={startCapture}>Start Capture</button>
-      <button onClick={stopCapture}>Stop Capture</button>
-      <video id="video" autoPlay className='w-[150px] h-[200px]'></video>
+    <div className='w-[300px] h-[400px] flex'>
+      <div className='flex flex-col justify-between py-3 items-center w-[15%] h-full bg-blue-700'>
+        <House />
+        <UserRoundPen />
+      </div>
+      <div className="w-full h-full flex flex-col items-center gap-4 bg-blue-500 justify-end py-10 ">
+        <img src="logo-2.png" alt="" />
+        <button onClick={startCapture} className='bg-white px-5 py-3 shadow-lg rounded-lg'>Start Capture</button>
+        <button onClick={stopCapture} className='bg-white px-5 py-3 rounded-lg shadow-lg'>Stop Capture</button>
+      </div>
     </div>
   );
 }
